653e0e70e5e11d2b48172da69d0e90d959f6903f
Melanie Junhuen Nuñez Santillan <mnunezs2100@alumno.ipn.mx>
2026-05-14 11:15:47 -0600

    Agrega cambios

---
+++ src/main/java/com/debuggeandoideas/erp_lite/ErpLiteApplication.java
@@ -0,0 +1,15 @@
+package com.debuggeandoideas.erp_lite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpLiteApplication.class, args);
+		System.out.println("Inicio de version");
+		System.out.println("Hola mundo");
+	}
+
+}
